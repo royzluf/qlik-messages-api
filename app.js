8fff67928952876133905b41fd5b29fe102a83ab
roy <royzluf@gmail.com>
2022-05-25 10:21:42 +0300

    Fix app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
@@ -11,24 +10,8 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-// Database not connected error
-app.use((_, res, next) => {
-    if (!mongoose.connection.readyState) {
-        res.status(500).json({ status: 'fail', message: 'Database not connected' });
-        return;
-    }
-    next();
-});
-
 // Parse request.body
 app.use(express.json());
-
-app.use((req, _, next) => {
-    req.requestTime = new Date().toISOString();
-    // console.log(req.method);
-    // console.log(req.get('User-Agent'));
-    next();
-});
 
 //#endregion Middlewares
 
